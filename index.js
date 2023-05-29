1094ffea3e982a48763c56a3ae28be52f5daa898
yuuyazw <120688819+yuuyazw@users.noreply.github.com>
2023-05-29 17:18:48 +0900

    イベント　発火基礎編
    nullと検証の時にConsoleに表示されてしまう。

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener('mouseenter',function(){
+    const pullDownButton=document.getElementById("Lists")
+});
+const pullDownButton=document.getElementById("Lists")
+console.log(pullDownButton);
